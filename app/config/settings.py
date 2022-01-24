e4440d6311635b805184ba9d56d7f98740cadb44
Joe Bullard <joekbullard@gmail.com>
2022-01-24 21:33:57 +0000

    docker init

--- app/config/settings.py
+++ app/config/settings.py
@@ -31,12 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # standard
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+
+    # third party
+    
+    # my apps
+    'peatdash',
 ]
 
 MIDDLEWARE = [
